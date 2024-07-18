c2c7c5263e0ea07acbc70f5e596b166434a1f352
Crackvignoule <kiki.kalagan@gmail.com>
2024-07-18 16:20:45 +0200

    renamed routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createUser, findUser, updateUserPassword, deleteUser } = require('../models/User');
 
 // Create a User
-router.post('/user', async (req, res) => {
+router.post('/users', async (req, res) => {
   try {
     const user = await createUser(req.body);
     res.status(201).json(user);
@@ -13,7 +13,7 @@ router.post('/user', async (req, res) => {
 });
 
 // Read a User by _id or username
-router.get('/user/:id', async (req, res) => {
+router.get('/users/:id', async (req, res) => {
   try {
     const user = await findUser(req.params.id);
     if (user == null) {
@@ -26,7 +26,7 @@ router.get('/user/:id', async (req, res) => {
 });
 
 // Update a User's password
-router.put('/user/:username', async (req, res) => {
+router.put('/users/:username', async (req, res) => {
   try {
     const user = await updateUserPassword(req.params.username, req.body.password);
     if (user == null) {
@@ -39,7 +39,7 @@ router.put('/user/:username', async (req, res) => {
 });
 
 // Delete a User
-router.delete('/user/:username', async (req, res) => {
+router.delete('/users/:username', async (req, res) => {
   try {
     const user = await deleteUser(req.params.username);
     if (user == null) {
